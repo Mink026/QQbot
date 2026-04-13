cb19eaac8c9f5146aeb31d5482b259317a7cacb3
JW <a446952980@gmail.com>
2026-04-13 15:54:52 +0800

    feat(dispatcher): 添加自定义表情获取和群消息发送功能
    - 实现了获取自定义表情的功能，设置数量为48
    - 集成了向指定群组发送消息的功能
    - 支持发送图片类型的消息内容
    - 配置了QQ表情包的URL路径
    - 设置了目标群组ID为996587913

--- src/handler/dispatcher.py
+++ src/handler/dispatcher.py
@@ -26,3 +26,37 @@ async def handle_event(event_data: dict, websocket: ClientConnection):
                 }
             )
         )
+        await websocket.send(
+            json.dumps(
+                {
+                    "action": "fetch_custom_face",
+                    "params": {
+                        "count": 48
+                    }
+                }
+            )
+        )
+        await websocket.send(
+            json.dumps(
+                {
+                    "action": "send_group_msg",
+                    "params": {
+                        "group_id": 996587913,
+                        "message": [
+                            {
+                                "type": "image",
+                                "data": {
+                                    # "path": "text",
+                                    # "thumb": "string",
+                                    # "name": "string",
+                                    "file": "Confused.png",
+                                    "url": "https://p.qpic.cn/qq_expression/994924990/994924990_0_0_0_2E9E7C076157138B8F40D2F80F70D5EA_0_0/0",
+                                    # "summary": "string",
+                                    # "sub_type": 0
+                                }
+                            }
+                        ]
+                    }
+                }
+            )
+        )
